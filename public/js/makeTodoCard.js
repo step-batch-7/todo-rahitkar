425059cafec1f1f570f61863d235cdca4cbc14dc
Rahit <rahitkar@users.noreply.github.com>
2020-02-04 13:32:23 +0530

    made ui more intarective and added ids in elements

--- public/js/makeTodoCard.js
+++ public/js/makeTodoCard.js
@@ -1,21 +1,21 @@
 const toHtml = function(title, items) {
   return (
-    `</br><div class="card">
-    <div class="todoHeader"><h4 style="color: rgba(0,0,0,0.7);">${title}</h4></div>` +
+    `<div class="todoHeader"><h4 style="color: rgba(0,0,0,0.7);">${title}</h4></div>` +
     items
       .map(item => {
-        return `<div class="todoItem">
-    <input type="checkbox"/><input type="text" name="todo item" class="textInput">${item.content}<br />
+        return `<div class="todoItem"  onmouseover="show() id="${item.id}"">
+    <input type="checkbox" id="${item.id}"/><label for="${item.id}">${item.content}</label><br />
   </div><br>`;
       })
-      .join('') +
-    `</div>`
+      .join('')
   );
 };
 
 const makeTodoCard = () => {
   const title = document.querySelector('#title').value;
   const todoItems = document.querySelector('#todoBox').value.split('\n');
+  document.querySelector('#title').value = '';
+  document.querySelector('#todoBox').value = '';
 
   const newTodoData = {
     title: title,
@@ -27,10 +27,12 @@ const makeTodoCard = () => {
   req.onload = function() {
     const todoList = document.querySelector('#todoList');
     const newTodo = document.createElement('div');
+    newTodo.className = 'card';
     const resText = JSON.parse(this.responseText);
+    newTodo.setAttribute('id', this.resText.id);
     newTodo.innerHTML = toHtml(resText.title, resText.tasks);
 
-    todoList.appendChild(newTodo);
+    todoList.prepend(newTodo);
   };
   req.open('POST', '/index.html');
   req.send(JSON.stringify(newTodoData));
@@ -41,13 +43,16 @@ const fetchAllTodoCards = () => {
 
   req.onload = function() {
     const todoList = document.querySelector('#todoList');
-    const newTodo = document.createElement('div');
 
     const allTodoCards = JSON.parse(this.responseText);
-    console.log(allTodoCards);
-    newTodo.innerHTML = allTodoCards.map((todoCard) => toHtml(todoCard.title, todoCard.tasks)) ;
-
-    todoList.appendChild(newTodo);
+    todoList.innerHTML = allTodoCards
+      .map(todoCard => {
+        return `<div class="card" id="${todoCard.id}">${toHtml(
+          todoCard.title,
+          todoCard.tasks
+        )}</div>`;
+      })
+      .join('');
   };
   req.open('GET', '/allTodo');
   req.send();
