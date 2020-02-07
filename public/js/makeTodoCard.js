d5e55b4df115f4a2de0ebf2a0f53fd5eacf048b6
Rahit <rahitkar@users.noreply.github.com>
2020-02-07 23:27:32 +0530

    add editable property in todo items in css

--- public/js/makeTodoCard.js
+++ public/js/makeTodoCard.js
@@ -1,7 +1,7 @@
 const toHtml = function(cardId, title, items) {
   const html =
     `
-    <div class="todoHeader"><h4  contenteditable="true" class="title";">${title}</h4></div><div class="crossDiv"><span onclick= removeTodo() class="crossButton">X</span></div>` +
+    <div class="todoHeader"><h4 contenteditable="true" class="title";">${title}</h4></div><div class="crossDiv"><span onclick= removeTodo() class="crossButton">X</span></div>` +
     `<div style="justify-content:flex-start; margin-top:10px;"><div id="todoss-${cardId}">` +
     items
       .map(item => {
@@ -23,12 +23,12 @@ const makeItemHtml = (cardId, item) => {
     return `
     <div class="todoItem" id="${id}">
       <input type="checkbox" onclick="toggleStatus('${cardId}', '${id}')" id="${id +
-      1}" checked/> &nbsp <label class="itemContent" for="${id}">${content}</label><span class="itemCrossButton" onclick="deleteItem('${cardId}', '${id}')"> &nbsp X</span></div><br /> 
+      1}" checked/> &nbsp <label contenteditable="true" class="itemContent" for="${id}">${content}</label><span class="itemCrossButton" onclick="deleteItem('${cardId}', '${id}')"> &nbsp X</span></div><br /> 
    `;
   }
   return `
   <div class="todoItem" id="${item.id}">
-  <input type="checkbox" onclick="toggleStatus('${cardId}', '${id}')"/> &nbsp <label class="itemContent" for="${id}">${content}</label><span onclick="deleteItem('${cardId}', '${id}')" class="itemCrossButton"> &nbsp X</span></div><br />
+  <input type="checkbox" onclick="toggleStatus('${cardId}', '${id}')"/> &nbsp <label contenteditable="true" class="itemContent" for="${id}">${content}</label><span onclick="deleteItem('${cardId}', '${id}')" class="itemCrossButton"> &nbsp X</span></div><br />
 `;
 };
 
