5de07ad5605cf11b03998b572d4889192958cd24
Rahit <rahitkar@users.noreply.github.com>
2020-02-08 10:52:01 +0530

    fix bug in search functions and refactored them

--- public/js/search.js
+++ public/js/search.js
@@ -1,10 +1,15 @@
-const searchByName = () => {
-  const titleElements = Array.from(document.querySelectorAll('.title'));
-  
-  const searchedName = document.querySelector('.nameSearch').value;
+const hideAllCards = () => {
   Array.from(document.querySelectorAll('.card')).forEach(
     card => (card.className = 'hide')
   );
+};
+
+const searchByName = () => {
+  const titleElements = Array.from(document.querySelectorAll('.todoTitle'));
+
+  const searchedName = document.querySelector('.nameSearch').value;
+
+  hideAllCards();
 
   titleElements.forEach(titleElement => {
     if (titleElement.value.match(new RegExp(searchedName))) {
@@ -14,20 +19,28 @@ const searchByName = () => {
   });
 };
 
+const iterate = function(iteratefn, value, times) {
+  if (times === 0) {
+    return value;
+  }
+  return iterate(iteratefn, iteratefn(value), times - 1);
+};
+
+const getParentElement = priValue => priValue.parentElement;
+
 const searchByTask = () => {
   const taskContentElements = Array.from(
     document.querySelectorAll('.itemContent')
   );
-  const searchedTask = document.querySelector('.nameSearch').value;
-  Array.from(document.querySelectorAll('.card')).forEach(
-    card => (card.className = 'hide')
-  );
+  const searchedTask = document.querySelector('.taskSearch').value;
+
+  hideAllCards();
+
   taskContentElements.forEach(taskContentElement => {
     if (taskContentElement.value.match(new RegExp(searchedTask))) {
-      taskContentElement.style.colour = 'red';
-      const card =
-        taskContentElement.parentElement.parentElement.parentElement
-          .parentElement.parentElement;
+      taskContentElement.style.backgroundColor = 'red';
+      const card = iterate(getParentElement, taskContentElement, 5);
+
       card.className = 'card';
     }
   });
